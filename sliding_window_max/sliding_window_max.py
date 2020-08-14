6d5b4d634e848555e3d8411f66d0fd0bd63c2534
john <john@spinningplates.org>
2020-08-13 23:56:46 -0700

    Completed sliding_window_max.py, first test pass, large test fails 25secs

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -1,15 +1,41 @@
-'''
+"""
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
-'''
+"""
+
+
 def sliding_window_max(nums, k):
     # Your code here
 
-    pass
+    j = k + 1
 
+    cur_max = max(nums[:k])
 
-if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    max_list = [cur_max]
+
+    for i in range(1, len(nums)):
+        if j <= len(nums):
+            cur_max = max(nums[i:j])
+            max_list.append(cur_max)
+            j += 1
+        else:
+            break
+
+    return max_list
+
+    # # runs in about 5 seconds
+    # cur_max = max(nums[:k])
+    # max_list = [cur_max]
+    # for i in range(k, len(nums)):
+    #     if nums[i] > cur_max:
+    #         cur_max = nums[i]
+    #     elif cur_max == nums[i - k]:
+    #         cur_max = max(nums[(i - k + 1):(i + 1)])
+    #     max_list.append(cur_max)
+    # return max_list
+
+if __name__ == "__main__":
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
